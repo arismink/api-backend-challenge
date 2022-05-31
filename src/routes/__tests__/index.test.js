49b9f62a8eb0ff445bfd850e4fe25f533dc384fa
arismink <krismina.la@gmail.com>
2022-05-31 00:57:25 -0600

    add second test

--- src/routes/__tests__/index.test.js
+++ src/routes/__tests__/index.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest')
 const app = require('../../../app');
+const routes = require('../posts')
 
 describe('Sanity test', () => {
   test('1 should equal 1', () => {
@@ -8,13 +9,25 @@ describe('Sanity test', () => {
 })
 
 describe('/GET posts', () => {
-  test('should return all posts tagged tech', async () => {
-    const res = await request(app)
-      .get('/api/posts/tech')
 
-    expect(res.statusCode).toEqual(200)
-    console.log(res._body);
+  test('should return all posts tagged tech', async () => {
+    const res = await request(app).get('/api/posts/tech')
+
+    // should return an object containing property 'posts'
     expect(res._body).toHaveProperty('posts');
+
+    // posts should equate to an array
     expect(Array.isArray(res._body.posts)).toBe(true)
-  })
+
+    // should not be an empty array
+    expect(res._body.posts.length).toBeGreaterThan(0)
+
+  });
+
+  test('should return error if tag is not present', async () => {
+    const res = await request(app).get('/api/posts/')
+
+    expect(res._body).toHaveProperty('error')
+  });
+
 })
