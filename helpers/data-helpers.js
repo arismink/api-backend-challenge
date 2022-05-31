6d09362aafb087ea27665ae228936814c1bd4929
arismink <krismina.la@gmail.com>
2022-05-30 21:28:49 -0600

    add sorting functions

--- helpers/data-helpers.js
+++ helpers/data-helpers.js
@@ -1,4 +1,50 @@
-const dataSort = (sortTag, direction) => {
+const sortById = (data, direction) => {
+  if (direction === "desc")
+    return data.sort((a, b) => a.id < b.id ? 1 : - 1);
+
+  return data.sort((a, b) => a.id > b.id ? 1 : - 1);
+
+};
+
+const sortByReads = (data, direction) => {
+  if (direction === "desc")
+    return data.sort((a, b) => a.reads < b.reads ? 1 : -1);
+
+  return data.sort((a, b) => a.reads > b.reads ? 1 : -1);
+};
+
+const sortByLikes = (data, direction) => {
+  if (direction === "desc")
+    return data.sort((a, b) => a.likes < b.likes ? 1 : -1);
+
+  return data.sort((a, b) => a.likes > b.likes ? 1 : -1)
+}
+
+const sortByPopularity = (data, direction) => {
+  if (direction === "desc")
+    return data.sort((a, b) => a.popularity < b.popularity ? 1 : -1);
+
+  return data.sort((a, b) => a.popularity > b.popularity ? 1 : -1);
+};
+
+const dataSort = (data, sortTag, direction) => {
+
+  switch(sortTag) {
+    case("id"):
+      return sortById(data, direction);
+
+    case("reads"):
+      return sortByReads(data, direction);
+
+    case("likes"):
+      return sortByLikes(data, direction);
+
+    case("popularity"):
+      return sortByPopularity(data, direction);
+
+    default:
+      return data;
+  }
 
 };
 
